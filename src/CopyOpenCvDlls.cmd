120e8e840bd9273c5ad5b721e4052777c6b2fa3d
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-02-15 16:55:56 +0900

    Fix typos in src/CopyOpenCvDlls.cmd

--- src/CopyOpenCvDlls.cmd
+++ src/CopyOpenCvDlls.cmd
@@ -1,10 +1,10 @@
-copy ..\DlibBuiltFilesOnWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibModifiedExamples\bin\x64\Release\
-copy ..\DlibBuiltFilesOnWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x64\Debug\
-copy ..\DlibBuiltFilesOnWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibModifiedExamples\bin\x86\Release\
-copy ..\DlibBuiltFilesOnWindows\opencv_x86\install\x86\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x86\Debug\
-copy ..\DlibBuiltFilesOnWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibExternTests\bin\x64\Release\
-copy ..\DlibBuiltFilesOnWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibExternTests\bin\x64\Debug\
-copy ..\DlibBuiltFilesOnWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibExternTests\bin\x86\Release\
-copy ..\DlibBuiltFilesOnWindows\opencv_x86\install\x86\vc14\bin\*320d.dll  DlibExternTests\bin\x86\Debug\
+copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibModifiedExamples\bin\x64\Release\
+copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x64\Debug\
+copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibModifiedExamples\bin\x86\Release\
+copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x86\Debug\
+copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibExternTests\bin\x64\Release\
+copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibExternTests\bin\x64\Debug\
+copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibExternTests\bin\x86\Release\
+copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320d.dll  DlibExternTests\bin\x86\Debug\
 
 pause
