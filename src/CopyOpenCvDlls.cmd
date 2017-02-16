ab82eabf444e1bd324058f0003f0bb3613fc578f
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-02-16 10:32:48 +0900

    Delete DlibModifiedExamples

--- src/CopyOpenCvDlls.cmd
+++ src/CopyOpenCvDlls.cmd
@@ -1,7 +1,3 @@
-copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibModifiedExamples\bin\x64\Release\
-copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x64\Debug\
-copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibModifiedExamples\bin\x86\Release\
-copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320d.dll  DlibModifiedExamples\bin\x86\Debug\
 copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320.dll   DlibExternTests\bin\x64\Release\
 copy ..\DlibBuiltFilesForWindows\opencv_x64\install\x64\vc14\bin\*320d.dll  DlibExternTests\bin\x64\Debug\
 copy ..\DlibBuiltFilesForWindows\opencv_x86\install\x86\vc14\bin\*320.dll   DlibExternTests\bin\x86\Release\
--- src/DlibModifiedExamples/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-
-#include "../DlibExtern/CommonSymbolsWithDlib.h"
-
-#define DlibBuiltFilesForWindows_ROOT_PATH  "../../DlibBuiltFilesForWindows/"
-
-#endif
